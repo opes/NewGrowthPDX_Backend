b3100858369ef63907f645cf81fde9a277af05c1
Derek Hudgins <hudgins.derek@gmail.com>
2021-10-06 11:54:09 -0700

    added sql tables

---
+++ sql/setup.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS plants CASCADE;
+DROP TABLE IF EXISTS greenhouse CASCADE;
+DROP TABLE IF EXISTS categories CASCADE;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username VARCHAR(256) NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE plants (
+    plant_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    plant_name  VARCHAR(256) NOT NULL,
+    plant_desc  VARCHAR(256)NOT NULL,
+    sci_name VARCHAR(256)
+    plant_img TEXT NOT NULL
+)
+
+CREATE TABLE greenhouse (
+    user_plant_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    users_id BIGINT REFERENCES users(id),
+    plant_id BIGINT REFERENCES plants(plant_id),
+    on_market BOOLEAN NOT NULL
+)
+
+CREATE TABLE categories (
+    name TEXT NOT NULL
+)
+
+
