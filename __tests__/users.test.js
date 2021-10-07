d816d27297649f63882d022e27b00bf8b1e0826d
Derek Hudgins <hudgins.derek@gmail.com>
2021-10-07 10:49:48 -0700

    swaping drivers

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
 // const User = require('../lib/models/User.js');
-const agent = request.agent(app);
+// const agent = request.agent(app);
 
 describe('tests all user routes', () => {
   beforeAll(() => {
@@ -13,16 +13,16 @@ describe('tests all user routes', () => {
   afterAll(() => {
     return pool.end();
   });
-  it('creates a user via POST', async () => {
-    const res = await agent.post('/api/v1/auth/signup').send({
-      username: 'testuser',
-      email: 'testuser@gmail.com',
-      password: 'qwerty',
-    });
-    expect(res.body).toEqual({
-      id: '1',
-      username: 'testuser',
-      email: 'testuser@gmail.com',
-    });
+  it('should signup a user', () => {
+    return request(app)
+      .post('/auth/signup')
+      .send({ username: 'testuser', email: 'testuser@gmail.com', password: 'qwerty' })
+      .then((res) => {
+        expect(res.body).toEqual({
+          id: '1',
+          username: 'testuser',
+          email: 'testuser@gmail.com',
+        });
+      });
   });
 });
