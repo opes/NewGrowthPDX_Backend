0d16052260fcd7697fd41d1995f362e11bcbd371
Sean <simpson.sean@outlook.com>
2021-10-10 14:24:31 -0700

    WORKDONE: Greenhouse testing

---
+++ __tests__/greenhouse.test.js
@@ -0,0 +1,60 @@
+const pool = require('../lib/utils/pool.js');
+const setup = require('../data/setup.js');
+const request = require('supertest');
+const app = require('../lib/app.js');
+const Plant = require('../lib/models/Plant');
+const Agent = request.agent(app);
+
+describe('test greenhouse', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('getting a users greenhouse', async () => {
+      const user = await request(app).post('/auth/signup').send({
+          email: 'joe@johndoh.com',
+          password: 'qwerty',
+          username: 'SomeDude',
+    })
+    
+      await Agent.post('/auth/login').send({
+        email: 'joe@johndoh.com',
+        password: 'qwerty',
+    });
+
+      console.log('LOOK HERE DUMBIES', user.body);
+
+      const plant1 = await Plant.insert({
+        plant_name: 'Tulip',
+        description: 'red',
+        scientific_name: '',
+        image: 'tulip.jpg',
+        category_id: '1',
+        userId: user.body.id,
+      });
+      const plant2 = await Plant.insert({
+        plant_name: 'Sunflower',
+        description: 'sunflower',
+        scientific_name: '',
+        image: 'sunflower.jpg',
+        category_id: '4',
+        userId: user.body.id,
+      });
+      const plant3 = await Plant.insert({
+        plant_name: 'Carnation',
+        description: 'The Dead loves this flower',
+        scientific_name: '',
+        image: 'carnation.jpg',
+        category_id: '1',
+        userId: user.body.id,
+      });
+
+      const response = await request(app).get(`/api/v1/greenhouse/${user.body.id}`);
+      expect(response.body).toEqual({ plant1, plant2, plant3 });
+
+ });
+});
