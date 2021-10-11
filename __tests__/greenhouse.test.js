c1e0e03cedd167d77016e529752db5b4565e9293
Derek Hudgins <hudgins.derek@gmail.com>
2021-10-11 10:02:23 -0700

    getsAllByUserId

--- __tests__/greenhouse.test.js
+++ __tests__/greenhouse.test.js
@@ -2,8 +2,8 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
-const Plant = require('../lib/models/Plant');
 const Agent = request.agent(app);
+const Plant = require('../lib/models/Plant');
 
 describe('test greenhouse', () => {
   beforeEach(() => {
@@ -26,36 +26,52 @@ describe('test greenhouse', () => {
       password: 'qwerty',
     });
 
-    console.log('LOOK HERE DUMBIES', user.body.id);
-
-    const plant1 = await Plant.insert({
+    const plant1 = {
       plant_name: 'Tulip',
       description: 'red',
       scientific_name: '',
       image: 'tulip.jpg',
-      category_id: '1',
+      category_id: '4',
       userId: user.body.id,
-    });
-    const plant2 = await Plant.insert({
+    };
+    const plant2 = {
       plant_name: 'Sunflower',
       description: 'sunflower',
       scientific_name: '',
       image: 'sunflower.jpg',
       category_id: '4',
       userId: user.body.id,
-    });
-    const plant3 = await Plant.insert({
-      plant_name: 'Carnation',
-      description: 'The Dead loves this flower',
+    };
+    const plant3 = {
+      plant_name: 'rose',
+      description: 'The Dead loves this flower sometimes',
       scientific_name: '',
-      image: 'carnation.jpg',
-      category_id: '1',
+      image: 'rose.jpg',
+      category_id: '2',
       userId: user.body.id,
-    });
+    };
 
-    const response = await request(app).get(
-      `/api/v1/greenhouse/${user.body.id}`
-    );
-    expect(response.body).toEqual({ plant1, plant2, plant3 });
+    const resPlant1 = await Plant.insert(plant1);
+    const resPlant2 = await Plant.insert(plant2);
+    const resPlant3 = await Plant.insert(plant3);
+
+    console.log(resPlant1, resPlant2, resPlant3);
+
+    // await Agent.post('/api/v1/plants').send(plant1);
+    // await Agent.post('/api/v1/plants').send(plant2);
+    // await Agent.post('/api/v1/plants').send(plant3);
+
+    // const response = await request(app).get(
+    //   `/api/v1/greenhouse/${user.body.id}`
+    // );
+
+    const response = await Plant.getAllPlantsByUserId(user.body.id);
+    console.log(response, 'look here');
+
+    expect(response).toEqual([
+      { ...plant1, id: '1' },
+      { ...plant2, id: '2' },
+      { ...plant3, id: '3' },
+    ]);
   });
 });
